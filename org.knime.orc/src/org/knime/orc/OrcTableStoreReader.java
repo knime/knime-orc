70e0794b9dddc490738c35fd47a2facba3158394
Marc Bux <marc.bux@knime.com>
2018-06-12 12:35:18 +0200

    AP-8748: Adjust in ORC table store to changed interfaces
    AP-8748 (Column-based storage: Support more than just native types)

--- org.knime.orc/src/org/knime/orc/OrcTableStoreReader.java
+++ org.knime.orc/src/org/knime/orc/OrcTableStoreReader.java
@@ -119,7 +119,7 @@ public final class OrcTableStoreReader extends AbstractTableStoreReader {
      *             of a writer has been used which hasn't been installed on the current system.
      */
     @Override
-    public void readMetaFromFile(final NodeSettingsRO settings, final int version)
+    protected void readMetaFromFile(final NodeSettingsRO settings, final int version)
         throws IOException, InvalidSettingsException {
         try {
             final List<OrcType<?>> types = new ArrayList<>();
